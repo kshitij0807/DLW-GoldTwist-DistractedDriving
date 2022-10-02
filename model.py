2ba2bde4314f9e661a714185a7641219fe991e2b
sjoker007 <kshitija001@e.ntu.edu.sg>
2022-10-03 00:35:14 +0800

    Dataset for training

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ import random,shutil
 from keras.models import Sequential
 from keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
 from keras.models import load_model
+import splitfolders
 
 
 def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
@@ -15,15 +16,13 @@ def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,ba
 
 BS= 32
 TS=(24,24)
-train_batch= generator('data/train',shuffle=True, batch_size=BS,target_size=TS)
-valid_batch= generator('data/valid',shuffle=True, batch_size=BS,target_size=TS)
-SPE= len(train_batch.classes)//BS
+train_batch= generator('dataset_new/train_data/train',shuffle=True,batch_size = BS,target_size=TS)
+valid_batch= generator('dataset_new/train_data/val',shuffle=True,batch_size = BS,target_size=TS)
+test_batch = generator('Dataset/test',shuffle = False, target_size=TS)
+SPE= len(test_batch.classes)//BS
 VS = len(valid_batch.classes)//BS
 print(SPE,VS)
 
-
-# img,labels= next(train_batch)
-# print(img.shape)
 
 model = Sequential([
     Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(24,24,1)),
@@ -52,6 +51,31 @@ model = Sequential([
 
 model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
-model.fit_generator(train_batch, validation_data=valid_batch,epochs=15,steps_per_epoch=SPE ,validation_steps=VS)
+model.fit_generator(train_batch, validation_data=valid_batch,epochs=50,steps_per_epoch=SPE ,validation_steps=VS)
 
+# Epoch 30/30
+# 30/30 [==============================] - 3s 95ms/step - loss: 0.0186 - accuracy: 0.9948 - val_loss: 0.1252 - val_accuracy: 0.9598
+# Epoch 50/50
+# 30/30 [==============================] - 3s 103ms/step - loss: 2.1422e-04 - accuracy: 1.0000 - val_loss: 0.2001 - val_accuracy: 0.9688
 model.save('models/cnnCat2.h5', overwrite=True)
+
+#history = model.fit(train_generator, epochs=50, validation_data=test_generator, shuffle=True, validation_steps=len(test_generator))
+#model.predict(test_batch)
+
+# accuracy = history.history['accuracy']
+# val_accuracy = history.history['val_accuracy']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs = range(len(accuracy))
+
+# plt.plot(epochs, accuracy, "b", label="trainning accuracy")
+# plt.plot(epochs, val_accuracy, "r", label="validation accuracy")
+# plt.legend()
+# plt.show()
+
+# plt.plot(epochs, loss, "b", label="trainning loss")
+# plt.plot(epochs, val_loss, "r", label="validation loss")
+# plt.legend()
+# plt.show()
+
+#predict = model.predict('')
